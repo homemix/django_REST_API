09d72c6a1ed1b7cd74a2fda3d37822aad135fc63
homemix <kenmutati@gmail.com>
2022-07-22 11:50:24 +0300

    initial files

---
+++ README.md
@@ -0,0 +1,4 @@
+## A Django Rest Frame Work Project
+
+<p>This project covers the following aspects:
+<li> Django rest framework and Json data display
---
+++ bibliophiles_api/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BibliophilesApiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'bibliophiles_api'
