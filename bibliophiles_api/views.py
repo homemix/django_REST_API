0fb6e74304f76342212800a7c3872132d480a274
homemix <kenmutati@gmail.com>
2022-07-31 13:04:55 +0300

    Add get query parameters

--- bibliophiles_api/views.py
+++ bibliophiles_api/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework import viewsets
+from rest_framework.decorators import action
+
 from .serializers import BookSerializer, GenreSerializer, ReviewSerializer
 from .models import Book, Genre, Review
 
@@ -13,6 +15,17 @@ class BookViewSet(viewsets.ModelViewSet):
     """
     queryset = Book.objects.all().order_by('published_date')
     serializer_class = BookSerializer
+
+    def get_queryset(self):
+        """
+        Optionally restricts the returned purchases to a given user,
+        by filtering against a `username` query parameter in the URL.
+        """
+        queryset = Book.objects.all()
+        title = self.request.query_params.get('title', None)
+        if title is not None:
+            queryset = queryset.filter(title__icontains=title)
+        return queryset
 
 
 class GenreSerializer(viewsets.ModelViewSet):
